2c73c91081a55d26f9758eb8d6d492af73724638
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-09-20 08:11:11 -0400

    made changes on intellij

--- src/BMI.java
+++ src/BMI.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class BMI {
-   test 2 
+   public static void main(String [] args){
+
+
+    int kg,wg;
+     Scanner keyboard;
+
+    //intilaize
+
+   keyboard=new Scanner(System.in);
+
+   //BMI=kg/m*m
+
+System.out.println("Please enter your weight in kilograms below");
+kg=keyboard.nextInt();
+
+System.out.println("Please enter your height in meters below");
+wg=keyboard.nextInt();
+
+
+
+
+       System.out.println("Your BMI is "+ (kg/wg)+"!");
+
+   }
 }

42d80027ff3be9523df425e8c7d34bd6259d1bbb
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-09-20 08:49:08 -0400

    made improvements

--- src/BMI.java
+++ src/BMI.java
@@ -5,9 +5,9 @@ public class BMI {
 
 
 
-    double feet;
-    double num;
-    
+    double height ;
+    double weight;
+
 
      Scanner keyboard;
 
@@ -18,13 +18,18 @@ public class BMI {
    //BMI=kg/m*m
 
 
-System.out.println("Please enter your height  below");
-feet=keyboard.nextDouble();
-feet*(1/0.025);
+System.out.println("Please enter your height(only feet) below");
+height =keyboard.nextDouble();
+height=height*(1/0.025);
+
+
+System.out.println("Please enter your weight(only pounds)below");
+weight=keyboard.nextDouble();
+weight= weight*(1/0.453592);
 
 
 
-
+       System.out.println("Your BMI is "+(weight/height));
 
 
    }
